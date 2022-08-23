da5e45b7ca85a0b45640dbe918b0dca1ba916679
Alex Via <alexandervia@gmail.com>
2022-08-23 14:21:41 -0500

    Adds dynamic create contact method

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,9 @@ export default {
   data: function() {
     return {
       message: "Contact List",
-      text: "These are your contacts",
-      contacts: []
+      text: "Add a contact",
+      contacts: [],
+      newContact: {}
     };
   },
   created: function() {
@@ -17,6 +18,15 @@ export default {
         console.log(response.data);
         this.contacts = response.data
       })
+    },
+    createContact: function() {
+      console.log("creating new contact")
+      axios.post("contacts", this.newContact).then(response =>{
+        console.log(response.data);
+      })
+    },
+    showContact: function() {
+      console.log("Showing contact info")
     }
   }
 };
@@ -25,14 +35,22 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <h3>{{ text }}</h3>
     <hr />
+    <!-- Add a new Contact -->
+    <h3>{{ text }}</h3>
+    <p><b>First Name:</b><input type="text" v-model="newContact.first_name"/></p>
+    <p><b>Last Name:</b><input type="text" v-model="newContact.last_name"/></p>
+    <p><b>Email:</b><input type="text" v-model="newContact.email"/></p>
+    <p><b>Phone:</b><input type="text" v-model="newContact.phone_number"/></p>
+    <p><b>Image:</b><input type="text" v-model="newContact.image"/></p>
+    <button v-on:click="createContact">Add new contact</button>
+    <hr />
+    <!-- Display Contacts -->
     <div v-for="contact in contacts">
     <p>{{ contact.first_name }}
     {{ contact.last_name }}</p>
-    <p>{{ contact.email }}</p>
-    <p>{{ contact.phone_number }}</p>
     <img :src="contact.image">
+    <button v-on:click="showContact()">More Info</button>
     <br>
     <hr />
     </div>
